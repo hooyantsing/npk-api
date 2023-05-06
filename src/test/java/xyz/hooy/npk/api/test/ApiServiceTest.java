ed7f232119a52cf6a65311046224c7385989bb67
hooyantsing <huyanqing@e-byte.com>
2023-05-06 22:16:52 +0800

    优化测试代码

--- src/test/java/xyz/hooy/npk/api/test/ApiServiceTest.java
+++ src/test/java/xyz/hooy/npk/api/test/ApiServiceTest.java
@@ -1,6 +1,7 @@
 package xyz.hooy.npk.api.test;
 
 import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import xyz.hooy.npk.api.ApiService;
 import xyz.hooy.npk.api.model.AbstractIndex;
@@ -8,7 +9,6 @@ import xyz.hooy.npk.api.model.Texture;
 import xyz.hooy.npk.api.util.TextureUtils;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
@@ -22,50 +22,40 @@ public class ApiServiceTest {
 
     @Test
     void getImgs() throws IOException {
-        ApiService apiService = ApiService.newInstance(INPUT_NPK_FILE);
-        Map<String, byte[]> imgs = apiService.getImgs();
-        for (String imgName : imgs.keySet()) {
-            System.out.println(imgName);
-        }
+        Map<String, byte[]> imgs = ApiService.newInstance(INPUT_NPK_FILE).getImgs();
+        long count = imgs.keySet().stream().peek(System.out::println).count();
+        Assertions.assertEquals(count, 11);
     }
 
     @Test
     void removeImg() throws IOException {
         Map<String, byte[]> imgs = ApiService.newInstance(INPUT_NPK_FILE).removeImg(1).getImgs();
-        for (String imgName : imgs.keySet()) {
-            System.out.println(imgName);
-        }
+        long count = imgs.keySet().stream().peek(System.out::println).count();
+        Assertions.assertEquals(count, 10);
     }
 
     @Test
     void getIndexs() throws IOException {
-        ApiService apiService = ApiService.newInstance(INPUT_NPK_FILE);
-        Map<String, List<AbstractIndex>> imgs = apiService.getIndexs();
-        for (Map.Entry<String, List<AbstractIndex>> img : imgs.entrySet()) {
-            System.out.println(img.getKey());
-            for (AbstractIndex index : img.getValue()) {
-                if (index instanceof Texture) {
-                    Texture texture = (Texture) index;
-                    System.out.println(texture);
-                }
-            }
-        }
+        Map<String, List<AbstractIndex>> indexs = ApiService.newInstance(INPUT_NPK_FILE).getIndexs();
+        indexs.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey());
+            entry.getValue().stream().forEach(System.out::println);
+        });
     }
 
     @Test
     void toPng() throws IOException {
-        ApiService apiService = ApiService.newInstance(INPUT_NPK_FILE);
-        Map<String, List<AbstractIndex>> imgs = apiService.getIndexs();
-        for (Map.Entry<String, List<AbstractIndex>> img : imgs.entrySet()) {
-            Path fileName = Paths.get(img.getKey()).getFileName();
-            System.out.println(fileName);
-            for (int i = 0; i < img.getValue().size(); i++) {
-                AbstractIndex index = img.getValue().get(i);
+        Map<String, List<AbstractIndex>> indexs = ApiService.newInstance(INPUT_NPK_FILE).getIndexs();
+        for (Map.Entry<String, List<AbstractIndex>> entry : indexs.entrySet()) {
+            String fileName = Paths.get(entry.getKey()).getFileName().toString();
+            for (int i = 0; i < entry.getValue().size(); i++) {
+                AbstractIndex index = entry.getValue().get(i);
                 if (index instanceof Texture) {
-                    Texture texture = (Texture) index;
-                    TextureUtils.toPng(texture, OUTPUT_PATH + fileName + "_" + i + ".png");
+                    TextureUtils.toPng((Texture) index, OUTPUT_PATH + fileName + "_" + i + ".png");
                 }
             }
+            System.out.println(fileName);
         }
     }
+
 }
