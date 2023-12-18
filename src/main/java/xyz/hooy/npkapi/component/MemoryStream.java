42e787fe93a289da9c749faadb11b092dc39f210
hooyantsing <hooyantsing@gmail.com>
2023-12-18 14:39:06 +0800

    [Update] Create MemoryStream Class.

--- src/main/java/xyz/hooy/npkapi/component/MemoryStream.java
+++ src/main/java/xyz/hooy/npkapi/component/MemoryStream.java
@@ -14,6 +14,8 @@ public class MemoryStream {
 
     private final ByteBuffer buffer;
 
+    private boolean writeFlip = true;
+
     public MemoryStream(int capacity) {
         this.buffer = ByteBuffer.allocate(capacity).order(ByteOrder.LITTLE_ENDIAN);
     }
@@ -29,48 +31,59 @@ public class MemoryStream {
     }
 
     public void read(byte[] bytes) {
+        autoReadFlip();
         buffer.get(bytes);
     }
 
     public byte readByte() {
+        autoReadFlip();
         return buffer.get();
     }
 
     public int readInt() {
+        autoReadFlip();
         return buffer.getInt();
     }
 
     public long readLong() {
+        autoReadFlip();
         return buffer.getLong();
     }
 
     public String readString() {
+        autoReadFlip();
         return readString(16);
     }
 
     public String readPath() {
+        autoReadFlip();
         return readString(256);
     }
 
     public String readString(int length) {
+        autoReadFlip();
         byte[] bytes = new byte[length];
         read(bytes);
         return StringUtils.toEncodedString(bytes, StandardCharsets.UTF_8).trim();
     }
 
     public void writeByte(byte b) {
+        autoWriteFlip();
         buffer.put(b);
     }
 
     public void write(byte[] bytes) {
+        autoWriteFlip();
         buffer.put(bytes);
     }
 
     public void writeInt(int value) {
+        autoWriteFlip();
         buffer.putInt(value);
     }
 
     public void writeLong(long value) {
+        autoWriteFlip();
         buffer.putLong(value);
     }
 
@@ -84,5 +97,19 @@ public class MemoryStream {
 
     public byte[] toArray() {
         return buffer.array();
+    }
+
+    private void autoReadFlip() {
+        if (writeFlip) {
+            buffer.flip();
+            writeFlip = false;
+        }
+    }
+
+    private void autoWriteFlip() {
+        if (!writeFlip) {
+            buffer.flip();
+            writeFlip = true;
+        }
     }
 }
